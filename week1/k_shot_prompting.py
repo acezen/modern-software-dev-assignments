fbb4eac5da0fcabd6f352ed62eb649b8c74372b5
acezen2 <15280508+acezen2@user.noreply.gitee.com>
2026-03-20 14:28:47 +0800

    fix prompt
    Signed-off-by: acezen2 <15280508+acezen2@user.noreply.gitee.com>

--- week1/k_shot_prompting.py
+++ week1/k_shot_prompting.py
@@ -8,19 +8,37 @@ NUM_RUNS_TIMES = 5
 
 # TODO: Fill this in!
 YOUR_SYSTEM_PROMPT = """
-You are a word reverser.Reverse the order of letters in the given words. Only output the reversed word, no other text." \
-Here are some examples:
+你是一个精确的字符处理专家。
+执行反转单词任务时，请严格遵守以下逻辑步骤：
+1. 将输入单词拆分为单个字母。
+2. 倒序排列这些字母。
+3. 将字母重新组合。
+
+只输出最终组合后的单词，不要输出中间过程。
+
+<example>
+input: container
+letters: c o n t a i n e r
+reverse: r e n i a t n o c
+output: reniatnoc
+</example>
 <example>
 input: hello
+letters: h e l l o
+reverse: o l l e h
 output: olleh
 </example>
 <example>
-input: world
-output: dlrow
+input: http
+letters: h t t p
+reverse: p t t h
+output: ptth
 </example>
 <example>
-input: python
-output: nohtyp
+input: status
+letters: s t a t u s
+reverse: s u t a t s
+output: sutats
 </example>
 """
 
